07e6722bf84e4e9484ffe08c38fe5f03446101a6
rudwk <tlsruwk12345@gmail.com>
2025-06-24 10:53:28 +0900

    node 파일 구조조정

--- node.js/func.js
+++
@@ -1,5 +0,0 @@
-const { odd, even } = require('./var');
-const checkOddOrEven = (num) => {
-return num % 2 ? odd : even;
-};
-module.exports = checkOddOrEven;
--- node.js/idex.js
+++
@@ -1,5 +0,0 @@
-const funcModule = require('./func');
-const varModule = require('./var');
-
-console.log(funcModule(10));
-console.log(varModule);
